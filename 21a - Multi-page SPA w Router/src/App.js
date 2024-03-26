7d88b44a50daf41495ca2923011f7fee2e6d9b3e
Tyler Sherman <67756633+TShermz@users.noreply.github.com>
2024-03-25 23:04:25 -0400

    Uploaded coursework projects/walkthroughs
    Included modules on Redux, Routing, Deployment, and full project demos

---
+++ 21a - Multi-page SPA w Router/src/App.js
@@ -0,0 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import RootLayout from "./pages/Root";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import ProductDetailPage from "./pages/ProductDetail";
+import ErrorPage from "./pages/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "products", element: <ProductsPage /> },
+      { path: "products/:productId", element: <ProductDetailPage /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
